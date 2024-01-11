5a66c6d1c195d7060eaa7816d0432ab507b03f8d
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-01-11 11:10:02 -0500

    comments practice

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,6 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -10,3 +10,13 @@
    comments. */
 
    console.log("Comments make your code more readable by others.");
+  
+
+   // this is a single line comment
+
+   /* this is a 
+   multi line comment */localStorage
+
+   console.log("TEST");
+   console.log(2 ** 2 ** 3 * 3);
+
