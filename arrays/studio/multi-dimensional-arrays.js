4f2b9fc7afbc73f1bf69e979f9fc1f541709ef50
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-01-20 20:33:11 -0500

    multi-dimensional arrays

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,28 @@
+const input = require("readline-sync");
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let foodArray = food.split(",").sort();
+let equipmentArray = equipment.split(",").sort();
+let petsArray = pets.split(",").sort();
+let sleepAidsArray = sleepAids.split(",").sort();
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [];
+cargoHold.push(foodArray,equipmentArray,petsArray,sleepAidsArray);
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+let theChosenCabinet = input.question("Choose a cabinet number 0 to 3.");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+if (theChosenCabinet > cargoHold.length - 1 || theChosenCabinet < 0) {
+    console.log(`Please choose a number from 0 to ${cargoHold.length - 1}`);
+} else {
+console.log(`The contents of the chosen cabinet ${theChosenCabinet} are ${cargoHold[theChosenCabinet].join(", ")}`);
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,10 +1,19 @@
-const input = require('readline-sync');
+const input = require("readline-sync");
 let str = "LaunchCode";
+
+let numOfLetters = input.question("Input how many letters to move. ");
+// Part 2 adding a if statment if suitable input
+if (numOfLetters > str.length || numOfLetters < 1) {
+    console.log(`You entered ${numOfLetters} letters to be moved but ${str} is only ${star.length} long. So we moved only 3 letters. `);
+    numOfLetters = 3;
+}
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let changeStr = str.slice(3) + str.slice(0,3);
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The old word was ${str} and the new word is ${changeStr}.`);
+
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
