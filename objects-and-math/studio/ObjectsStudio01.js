4cb9f3ee0a2ae3217b92be188469ae03540d72c3
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-02-01 21:37:28 -0500

    Ch. 12 Studio

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,35 @@
+// Randomly selected ID numbers STEP 1
 // Code your selectRandomEntry function here:
-
+// Function to pick index 0-5 of idNumbers array at random
+function selectRandomEntry(arr) {
+	let index = Math.floor(Math.random() * arr.length);
+	return arr[index];
+}
+// Randomly selected ID numbers STEP 2
+//avoid repeated ID numbers
+let threeCrewIds = [];
+let idNumbers = [291, 414, 503, 599, 796, 890];
+// keeps going until there are 3 unique IDs. 
+while (threeCrewIds.length < 3) {
+	let randomId = selectRandomEntry(idNumbers);
+	if (!threeCrewIds.includes(randomId)) {
+		threeCrewIds.push(randomId);
+	}
+}
 
 // Code your buildCrewArray function here:
+// Adds entire objects into selectedCrew
+function buildCrewArray(candidates, selectedIds) {
+	let selectedCrew = [];
+	for (let i=0; i < candidates.length; i++) {
+		if (selectedIds.includes(candidates[i].astronautID)) {
+			selectedCrew.push(candidates[i]);
+		}
+	}
+	return selectedCrew;
+}
 
 
-let idNumbers = [291, 414, 503, 599, 796, 890];
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -52,4 +77,8 @@ let candidateF = {
 
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
+// calling the buildCrewArray function and build a crew array
+let chosenCrew = buildCrewArray(animals, threeCrewIds);
+
 // Code your template literal and console.log statements:
+console.log(`${chosenCrew[0].name}, ${chosenCrew[1].name}, and ${chosenCrew[2].name} are going to space!\n`);
--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,14 +1,44 @@
-// Code your orbitCircumference function here:
-
-
+// Code your orbitCircumference function here: (C = 2 pie r )
+// orbit = radius
+//STEP 1.1
+function orbitCircumference() {
+orbit = 2000;
+return Math.round(2 * Math.PI * orbit);
+}
+// STEP 1.2, 1.3 ,1.4
 // Code your missionDuration function here:
-
+function missionDuration(numOrbits, radius = 2000, speed = 28000) {
+	let circumference = orbitCircumference(radius);
+	let distance = numOrbits * circumference;
+	// Rounding time
+	let time = Math.round(100 * distance / speed) / 100; 
+	console.log(`The mission will travel ${distance} km around the planet, and it will take ${time} hours to complete.`);
+	return time;
+}
+console.log(missionDuration(5));
 
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(arr) {
+	let index = Math.floor(Math.random() * arr.length);
+	return arr[index];
+}
+//avoid repeated ID numbers
+let threeCrewIds = [];
+let idNumbers = [291, 414, 503, 599, 796, 890];
+// keeps going until there are 3 unique IDs. 
+while (threeCrewIds.length < 3) {
+	let randomId = selectRandomEntry(idNumbers);
+	if (!threeCrewIds.includes(randomId)) {
+		threeCrewIds.push(randomId);
+	}
+}
 
 // Code your oxygenExpended function here:
-
+function oxygenExpended(candidate, alt = 2000, speed = 28000) {
+	let duration = missionDuration(3, alt, speed);
+	let oxygen = Math.round(candidate.o2Used(duration) * 1000) / 1000;
+	return `${candidate.name} will perform the spacewalk, which will last ${duration} hours and require ${oxygen} kg of oxygen.\n`
+}
 
 // Candidate data & crew array.
 let candidateA = {
@@ -54,5 +84,10 @@ let candidateA = {
    'astronautID':890
  };
  
- let crew = [candidateA,candidateC,candidateE];
+ let chosenCrew = [candidateA,candidateC,candidateE];
  
+
+ // STEP 2.4
+ // Select a crew member randomly and print their oxygen expenditure
+let selectedCandidate = selectRandomEntry(chosenCrew);
+console.log(oxygenExpended(selectedCandidate));
