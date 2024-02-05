be2deb02e2c6cd9a4f94869a27ca256fd1f8c0bd
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-02-04 20:26:05 -0500

    updated

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,63 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+// a
+console.log("PART 1-A");
+for (let i = 0; i <= 20; i++){
+console.log(i);
+}
+// b
+console.log("PART 1-B");
+for (let i = 3; i >=3 && i <= 29; i ++){
+    if (i % 2 === 1){
+    console.log(i); 
+  }
+}
+// c
+console.log("PART 1-C");
+for (let i = 12; i <= 12 && i >= -14; i --){
+    console.log(i);
+}
+// d
+console.log("PART 1-D");
+for ( let i = 50; i <= 50 && i >= 20; i --){
+  if ( i % 3 === 0){
+    console.log(i);
+  }
+}
 
+//*Exercise #2: 
+console.log("PART 2");
+//Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+let launch = "LaunchCode";
+let lcArray = [1, 5, "LC101", "blue", 42];
 
+// Construct ``for`` loops to accomplish the following tasks:
+//   a. Print each element of the array to a new line.
+for (let i = 0; i < lcArray.length; i++ ) {
+  console.log(lcArray[i]);
+}
+//   b. Print each character of the string - in reverse order - to a new line. */ 
+for (let i = 0; i < launch.length; i++){
+  let reverseLaunch = launch.split("").reverse().join("");
+  console.log(reverseLaunch[i]);
+}
 
-
-/*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
-
-Construct ``for`` loops to accomplish the following tasks:
-  a. Print each element of the array to a new line.
-  b. Print each character of the string - in reverse order - to a new line. */ 
-
-
-
-
-
-/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
-  a. One array contains the even numbers, and the other holds the odds.
-  b. Print the arrays to confirm the results. */
+console.log("PART 3");
+//*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+let daArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evenArray = [];
+let oddArray = [];
+  // a. One array contains the even numbers, and the other holds the odds.
+  for (let i = 0; i < daArray.length; i++){
+    if (daArray[i] % 2 === 0 ){
+      evenArray = evenArray.concat(daArray[i]);
+    } 
+    else {
+     oddArray = oddArray.concat(daArray[i]);
+    }
+  }
+  // b. Print the arrays to confirm the results. 
+  console.log(evenArray);
+  console.log(oddArray);
+  
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,46 @@
+const input = require("readline-sync");
+
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
-
+let startingFuel = ("");
+let numberOfAstronauts = ("");
+let altitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-
-
-
-
+startingFuel = input.question("What is the starting fuel? ");
+startingFuel = Number(startingFuel);
+while (startingFuel < 5000 || startingFuel > 30000 || isNaN(startingFuel)) {
+    startingFuel = input.question("Invalid!! Please enter value between 5,000 and 30,000. ");
+    startingFuel = Number(startingFuel);
+}
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
+numberOfAstronauts = input.question("How many astronauts? 1-7 ");
+numberOfAstronauts = Number(numberOfAstronauts);
+
+while (numberOfAstronauts < 1 || numberOfAstronauts > 7 || isNaN(numberOfAstronauts)) {
+  numberOfAstronauts = Number(input.question("Please enter a number 1 - 7. "));
+  //numberOfAstronauts = Number.numberOfAstronauts; 
+  // !!!WTF Removing "numberOfAstronauts = inputs.question("Please enter a number 1 - 7. ") " worked. it was doing a infinite loop when after the "input statement". ??? WHY?? Having the "Number" method on a seperate line caused it to loop. why? MAYBE it lets (input...) do its thing then change it to number then put int numberOfAstronauts. But the other way should have worked too. WEIRD
   
-  
-  
+}
+console.log(numberOfAstronauts); 
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
+while (startingFuel - 100 * numberOfAstronauts >= 0) {
+  altitude = altitude + 50;
+  startingFuel = startingFuel - (100 * numberOfAstronauts);
+}
 
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+console.log("The Shuttle gaind an altitude of " + altitude + " km");
+
+if (altitude >= 2000) {
+  console.log("ORBIT ACHIEVED!!!!")
+} else {
+  console.log("Failed to reach orbit. : ( ");
+}
+
