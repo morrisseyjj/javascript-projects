36414ed7929a5934a95e92b6a5ffa863fc892a13
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-02-08 22:50:48 -0500

    Ch. 14 Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,10 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirctor: "Jeff",
+  percentageCoolEmployees: "100",
+  programsOffered: ["LC101", "LaunchCode Women+", "CodeCamp"],
+
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,30 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+// ORG
+  it("should have a key called 'organization' with the value 'nonprofit'", function() {
+		expect(launchcode.organization).toEqual('nonprofit');
+	});
+  // EXE
+  it("should have a key called 'executiveDirector' with the value of 'Jeff'",
+  function(){
+    expect(launchcode.executiveDirctor).toEqual("Jeff")
+  });
+  // percentage
+  it("should have a key called 'percentageCoolEmployees' with the value of 100", function(){
+    expect(launchcode.percentageCoolEmployees).toEqual("100")
+  });
+// programsOffered
+  it('should have a key "programsOffered" with the value ["LC101", "LaunchCode Women+", "CodeCamp"]', function() {
+    expect(launchcode.programsOffered).toContain('LC101');
+    expect(launchcode.programsOffered).toContain('LaunchCode Women+');
+    expect(launchcode.programsOffered).toContain('CodeCamp');
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
+// launchOutPut
+
+
+
+
   
 });
